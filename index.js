3d47fb355c66167fd8ab57d6aebebf7f984fe71b
Miguel <miguelrooslencini@gmail.com>
2024-09-13 10:25:11 -0300

    adiciona inquirer

--- index.js
+++ index.js
@@ -1,18 +1,3 @@
-// let meta = {
-//     value: 'Ler um livro por mês',
-//     checked: true,
-// }
-
-// let metas = [
-//     meta,
-//     {
-//         value: "caminhar 20 mintos todos os dias",
-//         checked: false
-//     }
-// ]
-
-// console.log(metas[1].value);
-
 const start = () => {
     
     while(true) {
