7c0786d7db375b3a43be6ce7ed4554a79cae3873
Miguel <miguelrooslencini@gmail.com>
2024-09-13 10:12:34 -0300

    fundamentos 004

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
-let meta = {
-    value: 'Ler um livro por mês',
-    checked: true,
+// let meta = {
+//     value: 'Ler um livro por mês',
+//     checked: true,
+// }
+
+// let metas = [
+//     meta,
+//     {
+//         value: "caminhar 20 mintos todos os dias",
+//         checked: false
+//     }
+// ]
+
+// console.log(metas[1].value);
+
+const start = () => {
+    let count = 1
+    while(count <= 10) {
+        console.log(count);
+        count = count + 1
+        
+    }
+    
 }
 
-let metas = [
-    meta,
-    {
-        value: "caminhar 20 mintos todos os dias",
-        checked: false
-    }
-]
-
-console.log(metas[1].value);
+start();
